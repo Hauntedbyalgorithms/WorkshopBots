6da9ed9e1b95d4795147a586c33e0c621055d99f
jguess <lists@guess.fr>
2015-11-05 11:48:46 +0100

    com

--- twitterbot_model_additional_functions/tweeter.js
+++ twitterbot_model_additional_functions/tweeter.js
@@ -18,6 +18,7 @@ Tweeter.prototype.postTweet = function(txt) {
   this.T.post('statuses/update', { status: txt}, function(err, reply) {});
 };
 
+
 Tweeter.prototype.searchTweet = function(requete) {
 	this.T.get('search/tweets', { q: requete }, function(err, data, response) {
 		console.log(data);
